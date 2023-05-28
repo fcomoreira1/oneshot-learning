19a201aa63065328917fd79dbbf2c2381192d848
the-nguyen <nguyentrungthentt@gmail.com>
2023-05-28 16:03:39 +0200

    trains on emnist

--- methods.py
+++ methods.py
@@ -1,13 +1,17 @@
 import emnist
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import tensorflow.keras as keras
 from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from tensorflow.keras import layers
+from tensorflow.keras import optimizers
+from tensorflow.keras import metrics
+from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Input, Lambda, Conv2D, MaxPooling2D, BatchNormalization, Dense, Flatten, Activation, Dropout
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras import backend as K
 
 ''' Data-handling functions '''
 
@@ -94,11 +98,37 @@ def get_emnist(seed, n_train_classes, n_test_classes, ns_shots, verbose=True, re
 
 #     return train_images, train_labels, fewshot_images, fewshot_labels, validation_images, validation_labels
 
+''' Auxiliary functions '''
+
 def train_fewshot(encoder, n_shots, fewshot_images, fewshot_labels):
     return KNeighborsClassifier(n_neighbors=min(n_shots, 5)).fit(encoder(fewshot_images), fewshot_labels)
 
+def trains_fewshot(encoder, ns_shots, test_data, verbose=True):
+    accuracies = [None] * len(ns_shots)
+    for i, n_shots in enumerate(ns_shots):
+        if verbose: print(f'Learning {n_shots}-shot and predicting...')
+        fewshot_images, fewshot_labels, val_images, val_labels = test_data[i]
+        classifier = train_fewshot(encoder, n_shots, fewshot_images, fewshot_labels)
+        pred = classifier.predict(encoder(val_images))
+        accuracies[i] = np.sum(pred == val_labels) / val_labels.shape[0]
+        if verbose:
+            print(f'Accuracy for {n_shots}-shot: {accuracies[i]}')
+    return accuracies
+
 def train_fewshot_2(encoder, n_shots, fewshot_images, fewshot_labels):
     return KNeighborsClassifier(n_neighbors=min(n_shots, 5)).fit(encoder.transform(fewshot_images), fewshot_labels)
+
+def trains_fewshot_2(encoder, ns_shots, test_data, verbose=True):
+    accuracies = [None] * len(ns_shots)
+    for i, n_shots in enumerate(ns_shots):
+        if verbose: print(f'Learning {n_shots}-shot and predicting...')
+        fewshot_images, fewshot_labels, val_images, val_labels = test_data[i]
+        classifier = train_fewshot_2(encoder, n_shots, fewshot_images, fewshot_labels)
+        pred = classifier.predict(encoder.transform(val_images))
+        accuracies[i] = np.sum(pred == val_labels) / val_labels.shape[0]
+        if verbose:
+            print(f'Accuracy for {n_shots}-shot: {accuracies[i]}')
+    return accuracies
 
 ''' Linear methods '''
 
@@ -110,15 +140,7 @@ def test_PCA(seed, n_train_classes, n_test_classes, ns_shots, encoding_size=32, 
     pca = PCA(n_components=encoding_size)
     pca.fit(X=train_images)
 
-    accuracies = [None] * len(ns_shots)
-    for i, n_shots in enumerate(ns_shots):
-        if verbose: print(f'Learning {n_shots}-shot and predicting...')
-        fewshot_images, fewshot_labels, val_images, val_labels = test_data[i]
-        classifier = train_fewshot_2(pca, n_shots, fewshot_images, fewshot_labels)
-        pred = classifier.predict(pca.transform(val_images))
-        accuracies[i] = np.sum(pred == val_labels) / val_labels.shape[0]
-        if verbose:
-            print(f'Accuracy for {n_shots}-shot: {accuracies[i]}')
+    accuracies = trains_fewshot_2(pca, ns_shots, test_data)
 
     if verbose:
         print("======= PCA method: Finished =======")
@@ -133,15 +155,7 @@ def test_LDA(seed, n_train_classes, n_test_classes, ns_shots, encoding_size=32, 
     lda = LDA(n_components=encoding_size)
     lda.fit(X=train_images, y=train_labels)
 
-    accuracies = [None] * len(ns_shots)
-    for i, n_shots in enumerate(ns_shots):
-        if verbose: print(f'Learning {n_shots}-shot and predicting...')
-        fewshot_images, fewshot_labels, val_images, val_labels = test_data[i]
-        classifier = train_fewshot_2(lda, n_shots, fewshot_images, fewshot_labels)
-        pred = classifier.predict(lda.transform(val_images))
-        accuracies[i] = np.sum(pred == val_labels) / val_labels.shape[0]
-        if verbose:
-            print(f'Accuracy for {n_shots}-shot: {accuracies[i]}')
+    accuracies = trains_fewshot_2(lda, ns_shots, test_data)
     
     if verbose:
         print("======= LDA method: Finished =======")
@@ -168,7 +182,7 @@ def nonlinear_autoencoder(input_size, code_size: int):
     outputs = decoder(encoder(inputs))
     autoencoder = keras.Model(inputs=inputs, outputs=outputs)
     
-    autoencoder.compile(optimizer='Adam', loss='MSE')
+    autoencoder.compile(optimizer=optimizers.legacy.Adam(), loss='MSE')
     return autoencoder, encoder
 
 def cnn_encoder(w, h, encoding_size):
@@ -206,53 +220,37 @@ def nonlinear_autoencoder_with_cnn_encoder(input_shape, code_size):
     outputs = decoder(encoder(inputs))
     autoencoder = keras.Model(inputs=inputs, outputs=outputs)
     
-    autoencoder.compile(optimizer='Adam', loss='MSE')
+    autoencoder.compile(optimizer=optimizers.legacy.Adam(), loss='MSE')
     return autoencoder, encoder
 
-def test_NLAE(seed, n_train_classes, n_test_classes, ns_shots, encoding_size=32, verbose=True):
+def test_NLAE(seed, n_train_classes, n_test_classes, ns_shots, n_epochs=15, encoding_size=32, verbose=True):
     train_images, train_labels, test_data = get_emnist(seed, n_train_classes, n_test_classes, ns_shots, False, True)
     
     if verbose: print("======= Nonlinear autoencoder method: Training and evaluating... =======")
     if verbose: print("Learning background...")
     autoencoder, encoder = nonlinear_autoencoder(train_images.shape[1], encoding_size)
-    autoencoder.fit(train_images, train_images, epochs=15)
+    autoencoder.fit(train_images, train_images, epochs=n_epochs)
 
-    accuracies = [None] * len(ns_shots)
-    for i, n_shots in enumerate(ns_shots):
-        if verbose: print(f'Learning {n_shots}-shot and predicting...')
-        fewshot_images, fewshot_labels, val_images, val_labels = test_data[i]
-        classifier = train_fewshot(encoder, n_shots, fewshot_images, fewshot_labels)
-        pred = classifier.predict(encoder(val_images))
-        accuracies[i] = np.sum(pred == val_labels) / val_labels.shape[0]
-        if verbose:
-            print(f'Accuracy for {n_shots}-shot: {accuracies[i]}')
+    accuracies = trains_fewshot(encoder, ns_shots, test_data)
     
     if verbose:
         print("======= Nonlinear autoencoder method: Finished =======")
 
     return accuracies
 
-def test_NLAE_CNNE(seed, n_train_classes, n_test_classes, ns_shots, encoding_size=32, verbose=True):
+def test_NLAE_CNNE(seed, n_train_classes, n_test_classes, ns_shots, n_epochs=15, encoding_size=32, verbose=True):
     train_images, train_labels, test_data = get_emnist(seed, n_train_classes, n_test_classes, ns_shots, False)
 
     if verbose: print("======= Nonlinear autoencoder with CNN encoder method: Training and evaluating... =======")
     if verbose: print("Learning background...")
     _, w, h = train_images.shape
     autoencoder, encoder = nonlinear_autoencoder_with_cnn_encoder((w, h, 1), encoding_size)
-    autoencoder.fit(train_images, train_images.reshape(-1, w * h), epochs=10)
+    autoencoder.fit(train_images, train_images.reshape(-1, w * h), epochs=n_epochs)
 
-    accuracies = [None] * len(ns_shots)
-    for i, n_shots in enumerate(ns_shots):
-        if verbose: print(f'Learning {n_shots}-shot and predicting...')
-        fewshot_images, fewshot_labels, val_images, val_labels = test_data[i]
-        classifier = train_fewshot(encoder, n_shots, fewshot_images, fewshot_labels)
-        pred = classifier.predict(encoder(val_images))
-        accuracies[i] = np.sum(pred == val_labels) / val_labels.shape[0]
-        if verbose:
-            print(f'Accuracy for {n_shots}-shot: {accuracies[i]}')
+    accuracies = trains_fewshot(encoder, ns_shots, test_data)
     
     if verbose:
-        print("======= Nonlinear autoencoder method: Finished =======")
+        print("======= Nonlinear autoencoder with CNN encoder method: Finished =======")
 
     return accuracies
 
@@ -331,7 +329,18 @@ def get_train_data(train_images, train_labels, batch_size):
         pairs[1][i] = get_image_by_label(train_images, train_labels, class2 if i < batch_size // 2 else class1)
     return pairs, targets
 
-def siamese_net_with_cnn_encoder(w, h, encoding_size):
+def get_triplets(train_images, train_labels, batch_size, w, h):
+    triplets = [np.zeros((batch_size, w, h)) for _ in range(3)]
+    labels = np.unique(train_labels)
+    for i in range(batch_size):
+        class1, class2 = np.random.choice(labels, 2, replace=False)
+        assert(class1 != class2)
+        triplets[0][i] = get_image_by_label(train_images, train_labels, class1)
+        triplets[1][i] = get_image_by_label(train_images, train_labels, class1)
+        triplets[2][i] = get_image_by_label(train_images, train_labels, class2)
+    return triplets
+
+def siamese_net(w, h, encoding_size):
     left_input = Input((w, h, 1))
     right_input = Input((w, h, 1))
     
@@ -344,9 +353,9 @@ def siamese_net_with_cnn_encoder(w, h, encoding_size):
     L1_Dist = L1_Layer([left_emb,right_emb])
     OP = Dense(1, activation='sigmoid', kernel_regularizer='l2')(L1_Dist)
     
-    siamese_net = Model(inputs=[left_input, right_input], outputs=OP)
+    siamese_network = Model(inputs=[left_input, right_input], outputs=OP)
     
-    return siamese_net, encoder
+    return siamese_network, encoder
 
 def test_SN(seed, n_train_classes, n_test_classes, ns_shots, n_iterations=3000, batch_size=10, encoding_size=32, verbose=True):
     train_images, train_labels, test_data = get_emnist(seed, n_train_classes, n_test_classes, ns_shots, False)
@@ -356,28 +365,141 @@ def test_SN(seed, n_train_classes, n_test_classes, ns_shots, n_iterations=3000, 
     if verbose: print("======= Siamese network method: Training and evaluating... =======")
     if verbose: print("Learning background...")
 
-    siamese_net, encoder = siamese_net_with_cnn_encoder(w, h, encoding_size)
-    siamese_net.compile(
+    siamese_network, encoder = siamese_net(w, h, encoding_size)
+    siamese_network.compile(
         loss='binary_crossentropy',
-        optimizer='Adam',
+        optimizer=optimizers.legacy.Adam(),
         metrics=['accuracy']
     )
 
     for i in range(n_iterations):
         x, y = get_train_data(train_images, train_labels, batch_size)
-        siamese_net.fit(x, y, verbose=False)
+        siamese_network.fit(x, y, verbose=False)
 
-    accuracies = [None] * len(ns_shots)
-    for i, n_shots in enumerate(ns_shots):
-        if verbose: print(f'Learning {n_shots}-shot and predicting...')
-        fewshot_images, fewshot_labels, val_images, val_labels = test_data[i]
-        classifier = train_fewshot(encoder, n_shots, fewshot_images, fewshot_labels)
-        pred = classifier.predict(encoder(val_images))
-        accuracies[i] = np.sum(pred == val_labels) / val_labels.shape[0]
-        if verbose:
-            print(f'Accuracy for {n_shots}-shot: {accuracies[i]}')
+    accuracies = trains_fewshot(encoder, ns_shots, test_data)
     
     if verbose:
-        print("======= Nonlinear autoencoder method: Finished =======")
+        print("======= Siamese network method: Finished =======")
+
+    return accuracies
+
+class DistanceLayer(layers.Layer):
+    """
+    This layer is responsible for computing the distance between the anchor
+    embedding and the positive embedding, and the anchor embedding and the
+    negative embedding.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, anchor, positive, negative):
+        ap_distance = tf.reduce_sum(tf.square(anchor - positive), -1)
+        an_distance = tf.reduce_sum(tf.square(anchor - negative), -1)
+        return (ap_distance, an_distance)
+
+def siamese_net_for_triplet_loss(w, h, encoding_size):
+    anchor_input = layers.Input(name="anchor", shape=(w, h, 1))
+    positive_input = layers.Input(name="positive", shape=(w, h, 1))
+    negative_input = layers.Input(name="negative", shape=(w, h, 1))
+
+    encoder = cnn_encoder(w, h, encoding_size)
+
+    distances = DistanceLayer()(
+        encoder(anchor_input),
+        encoder(positive_input),
+        encoder(negative_input),
+    )
+
+    siamese_network = Model(
+        inputs=[anchor_input, positive_input, negative_input], outputs=distances
+    )
+
+    return siamese_network, encoder
+
+class SiameseModel(Model):
+    """
+    The Siamese Network model with a custom training and testing loops.
+
+    Computes the triplet loss using the three embeddings produced by the
+    Siamese Network.
+
+    The triplet loss is defined as:
+       L(A, P, N) = max(‖f(A) - f(P)‖² - ‖f(A) - f(N)‖² + margin, 0)
+    """
+
+    def __init__(self, siamese_network, margin=5):
+        super().__init__()
+        self.siamese_network = siamese_network
+        self.margin = margin
+        self.loss_tracker = metrics.Mean(name="loss")
+
+    def call(self, inputs):
+        return self.siamese_network(inputs)
+
+    def train_step(self, data):
+        # GradientTape is a context manager that records every operation that
+        # you do inside. We are using it here to compute the loss so we can get
+        # the gradients and apply them using the optimizer specified in
+        # `compile()`.
+        with tf.GradientTape() as tape:
+            loss = self._compute_loss(data)
+
+        # Storing the gradients of the loss function with respect to the
+        # weights/parameters.
+        gradients = tape.gradient(loss, self.siamese_network.trainable_weights)
+
+        # Applying the gradients on the model using the specified optimizer
+        self.optimizer.apply_gradients(
+            zip(gradients, self.siamese_network.trainable_weights)
+        )
+
+        # Let's update and return the training loss metric.
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result(), "pdistance": self.siamese_network(data)[0], "ndistance": self.siamese_network(data)[1]}
+
+    def test_step(self, data):
+        loss = self._compute_loss(data)
+
+        # Let's update and return the loss metric.
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+    def _compute_loss(self, data):
+        # The output of the network is a tuple containing the distances
+        # between the anchor and the positive example, and the anchor and
+        # the negative example.
+        ap_distance, an_distance = self.siamese_network(data)
+
+        # Computing the Triplet Loss by subtracting both distances and
+        # making sure we don't get a negative value.
+        loss = ap_distance - an_distance
+        loss = tf.maximum(loss + self.margin, 0.0)
+        return loss
+
+    @property
+    def metrics(self):
+        # We need to list our metrics here so the `reset_states()` can be
+        # called automatically.
+        return [self.loss_tracker]
+
+def test_SN_TL(seed, n_train_classes, n_test_classes, ns_shots, n_iterations=2000, batch_size=10, encoding_size=32, verbose=True):
+    train_images, train_labels, test_data = get_emnist(seed, n_train_classes, n_test_classes, ns_shots, False)
+
+    _, w, h = train_images.shape
+
+    if verbose: print("======= Siamese network with triplet loss method: Training and evaluating... =======")
+    if verbose: print("Learning background...")
+
+    siamese_network, encoder = siamese_net_for_triplet_loss(w, h, encoding_size)
+    siamese_model = SiameseModel(siamese_network)
+    siamese_model.compile(optimizer=optimizers.legacy.Adam())
+    for _ in range(n_iterations):
+        siamese_model.fit(get_triplets(train_images, train_labels, batch_size, w, h), verbose=False)
+
+    accuracies = trains_fewshot(encoder, ns_shots, test_data)
+    
+    if verbose:
+        print("======= Siamese network with triplet loss method: Finished =======")
 
     return accuracies
